fd1a8c0d729963edf63e14005b9d1072d42562c8
pcc0105@gmail.com <pcc0105@gmail.com>
2023-03-10 15:24:14 +0900

    2023.03.10

--- src/Ch20/sec08/BoardDeleteExample.java
+++ src/Ch20/sec08/BoardDeleteExample.java
@@ -1,51 +1,42 @@
-package Ch20.sec07;
+package Ch20.sec08;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class BoardUpdateExample {
+public class BoardDeleteExample {
 	public static void main(String[] args) {
 		Connection conn = null;
 		try {
 			// JDBC Driver등록
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
-			// Connection객체 생성(연결하기)
+			// Connection객체 생성
 			conn = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/thisisjava",
 					"chun",
 					"1234");
-
 			System.out.println("연결 성공");
 
-			// --------------SQL------------
-			// 매개변수화된 SQL 문 작성
-			String sql = "update boards set " + "btitle = ?, " + "bcontent = ?, " + "bfilename = ?, "
-					+ "bfiledata= ? " + "where bno = ? ";
-			// PreparedStatement 얻기
+			// SQL문
+			String sql = "DELETE FROM boards WHERE bwriter = ?";
+
+			// PreparedSatement객체 생성
 			PreparedStatement pstmt = conn.prepareStatement(sql);
+
 			// 값넣기
-			pstmt.setString(1, "눈사람");
-			pstmt.setString(2, "눈으로 만든 사람");
-			pstmt.setString(3, "업데이트 해보기");
-			pstmt.setBlob(4, new FileInputStream("src/ch20/sec07/photo2.png"));
-			pstmt.setInt(5, 3);
+			pstmt.setString(1, "winter");
 
-			// sql문실행
+			// 실행
 			int rows = pstmt.executeUpdate();
-			System.out.println("업데이트된 행수: " + rows);
+			System.out.println("삭제된 행 수: " + rows);
 
-			// PreparedStatement 닫기
+			// 닫기
 			pstmt.close();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} finally {
 			if (conn != null) {
